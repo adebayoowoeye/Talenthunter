450e00ca5654cf766987871d81b4985be918bf59
Adebayo Owoeye <owoeye0075@gmail.com>
2024-09-24 22:49:49 +0100

    Server has now been setup and running

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Talenthunter
 
 This repository is for a project called "TalentHunter"
+
+```
+ npm install dotenv express mongoose express-async-handler bcryptjs handlebars jsonwebtoken morgan cookie-parser winston chalk winston-daily-rotate-file express-rate-limit
+```
+
+```
+npm i -D nodemon
+```
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,29 @@
-// console.log('Starting');
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import chalk from 'chalk';
+import morgan from 'morgan';
+import 'dotenv/config';
+
+// Create an Express app
+
+const app = express();
+
+// Logging middleware
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+  app.use(morgan('dev'));
+}
+// Middleware
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/api/v1/test', (req, res) => {
+  res.json({ message: { text: `API endpoint for testing` } });
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(
+    `${chalk.green.bold(`✅`)} Server running in ${chalk.blue.bold(process.env.NODE_ENV)} mode on port ${chalk.blueBright.bold(port)}`
+  );
+});
