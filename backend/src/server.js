7affb98a9336be47561f1edf77361728f3efa295
Adebayo Owoeye <owoeye0075@gmail.com>
2024-10-01 21:58:38 +0100

    Implement Logging logging_setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import cookieParser from 'cookie-parser';
 import chalk from 'chalk';
 import morgan from 'morgan';
 import 'dotenv/config';
+// eslint-disable-next-line import/extensions
+import { systLogs, morganMiddleware } from '../utils/Logger.js';
 
 // Create an Express app
 
@@ -17,13 +19,16 @@ if (process.env.NODE_ENV === 'DEVELOPMENT') {
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(morganMiddleware);
 
 app.get('/api/v1/test', (req, res) => {
   res.json({ message: { text: `API endpoint for testing` } });
 });
+
+const hostname = process.env.HOST_NAME || 'localhost';
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(
-    `${chalk.green.bold(`✅`)} Server running in ${chalk.blue.bold(process.env.NODE_ENV)} mode on port ${chalk.blueBright.bold(port)}`
+  systLogs.info(
+    `${chalk.green.bold(`✅`)} Server running in ${chalk.blue.bold(process.env.NODE_ENV)} mode on port ${chalk.blueBright.bold(port)} at http://${hostname}:${port}/`
   );
 });
