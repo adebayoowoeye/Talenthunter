5547229284dd3219e38d181bb00f27c2f5ebb8f9
Adebayo Owoeye <owoeye0075@gmail.com>
2024-10-26 12:04:55 +0100

    Modify create product routes

--- backend/src/routes/api/product.js
+++ backend/src/routes/api/product.js
@@ -1,11 +1,14 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { newProduct } from '../../controllers/productController.js';
+import { newProduct, getSingleProduct } from '../../controllers/productController.js';
 
 const router = express.Router();
 
 // create new product
 
 router.post('/admin/product/new', newProduct);
+
+// Get single product
+router.get('/product/:id', getSingleProduct);
 
 export default router;
